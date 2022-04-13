34b21572d9e072becd9abc4f4048f89ce9952e29
BIPLOB <developerbiplob.bd@gmail.com>
2022-04-14 02:56:38 +0600

    Animtion process

--- custom.js
+++ custom.js
@@ -7,6 +7,10 @@ const select = (ele)=> {return document.querySelector(ele)}
 //     select('#demu').style.fontSize = '30px';
 // }, 2000)
 
+
+
+// Single box Animatin-----//
+
 // function myMove(){
 
 //     const animate = select('#animate');
@@ -26,10 +30,59 @@ const select = (ele)=> {return document.querySelector(ele)}
 // }
 
 
+// Two box Animatin-----//
+
+// function myMove(){
+
+//     const animate = select('#animate');
+//     const animate2 = select('#animate2');
+
+ 
+//     let closeTime = null;
+//     // let setTime = null;
+
+//     let setTime = setInterval(frame, 5);
+
+    
+//     let pos = 0;
+//     function frame(){
+//         if(pos == 350){
+//             clearInterval(setTime);
+//             closeTime = setInterval(closeFarme, 5);
+//         }else{
+//             pos++
+//             animate.style.top = pos + 'px'
+//             animate.style.left = pos + "px"
+
+//             animate2.style.top = pos + 'px'
+//             animate2.style.right = pos + "px"
+//         }
+//     }
+
+//     function closeFarme(){
+//         if(pos == 0){
+//             clearInterval(closeTime);
+//             setTime = setInterval(frame, 5);
+
+//         }else{
+//             pos--
+//             animate.style.top = pos + 'px'
+//             animate.style.left = pos + "px"
+
+//             animate2.style.top = pos + 'px'
+//             animate2.style.right = pos + "px"
+//         }
+//     }
+// }
+
+// // Foure box Animatin-----//
+
 function myMove(){
 
     const animate = select('#animate');
     const animate2 = select('#animate2');
+    const animate3 = select('#animate3');
+    const animate4 = select('#animate4');
 
  
     let closeTime = null;
@@ -50,6 +103,12 @@ function myMove(){
 
             animate2.style.top = pos + 'px'
             animate2.style.right = pos + "px"
+
+            animate3.style.bottom = pos + 'px'
+            animate3.style.left = pos + "px"
+
+            animate4.style.bottom = pos + 'px'
+            animate4.style.right = pos + "px"
         }
     }
 
@@ -65,6 +124,12 @@ function myMove(){
 
             animate2.style.top = pos + 'px'
             animate2.style.right = pos + "px"
+
+            animate3.style.bottom = pos + 'px'
+            animate3.style.left = pos + "px"
+
+            animate4.style.bottom = pos + 'px'
+            animate4.style.right = pos + "px"
         }
     }
 }
