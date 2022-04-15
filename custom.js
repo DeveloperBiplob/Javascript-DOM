8e188dd4190df02016cce64d982a160cb12e90f8
BIPLOB <developerbiplob.bd@gmail.com>
2022-04-15 23:35:59 +0600

    some modify

--- custom.js
+++ custom.js
@@ -77,62 +77,69 @@ const select = (ele)=> {return document.querySelector(ele)}
 
 // // Foure box Animatin-----//
 
-function myMove(){
+// function myMove(){
 
-    const animate = select('#animate');
-    const animate2 = select('#animate2');
-    const animate3 = select('#animate3');
-    const animate4 = select('#animate4');
+//     const animate = select('#animate');
+//     const animate2 = select('#animate2');
+//     const animate3 = select('#animate3');
+//     const animate4 = select('#animate4');
 
  
-    let closeTime = null;
-    // let setTime = null;
+//     let closeTime = null;
+//     // let setTime = null;
 
-    let setTime = setInterval(frame, 5);
+//     let setTime = setInterval(frame, 5);
 
     
-    let pos = 0;
-    function frame(){
-        if(pos == 350){
-            clearInterval(setTime);
-            closeTime = setInterval(closeFarme, 5);
-        }else{
-            pos++
-            animate.style.top = pos + 'px'
-            animate.style.left = pos + "px"
+//     let pos = 0;
+//     function frame(){
+//         if(pos == 350){
+//             clearInterval(setTime);
+//             closeTime = setInterval(closeFarme, 5);
+//         }else{
+//             pos++
+//             animate.style.top = pos + 'px'
+//             animate.style.left = pos + "px"
 
-            animate2.style.top = pos + 'px'
-            animate2.style.right = pos + "px"
+//             animate2.style.top = pos + 'px'
+//             animate2.style.right = pos + "px"
 
-            animate3.style.bottom = pos + 'px'
-            animate3.style.left = pos + "px"
+//             animate3.style.bottom = pos + 'px'
+//             animate3.style.left = pos + "px"
 
-            animate4.style.bottom = pos + 'px'
-            animate4.style.right = pos + "px"
-        }
-    }
+//             animate4.style.bottom = pos + 'px'
+//             animate4.style.right = pos + "px"
+//         }
+//     }
 
-    function closeFarme(){
-        if(pos == 0){
-            clearInterval(closeTime);
-            setTime = setInterval(frame, 5);
+//     function closeFarme(){
+//         if(pos == 0){
+//             clearInterval(closeTime);
+//             setTime = setInterval(frame, 5);
 
-        }else{
-            pos--
-            animate.style.top = pos + 'px'
-            animate.style.left = pos + "px"
+//         }else{
+//             pos--
+//             animate.style.top = pos + 'px'
+//             animate.style.left = pos + "px"
 
-            animate2.style.top = pos + 'px'
-            animate2.style.right = pos + "px"
+//             animate2.style.top = pos + 'px'
+//             animate2.style.right = pos + "px"
 
-            animate3.style.bottom = pos + 'px'
-            animate3.style.left = pos + "px"
+//             animate3.style.bottom = pos + 'px'
+//             animate3.style.left = pos + "px"
 
-            animate4.style.bottom = pos + 'px'
-            animate4.style.right = pos + "px"
-        }
-    }
-}
+//             animate4.style.bottom = pos + 'px'
+//             animate4.style.right = pos + "px"
+//         }
+//     }
+// }
+
+
+// -------
+
+// video 96
+
+// Vide0 100//
 
 
 
